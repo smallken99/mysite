9806aaa41b93f8a027bb220ac704a11b963480f4
smallken99 <smallken@gmail.com>
2018-10-20 00:30:57 +0800

    待繳費優化小修

--- polls/urls.py
+++ polls/urls.py
@@ -3,20 +3,20 @@ from . import views
 
 
 urlpatterns = [
-    # ex: /polls/ 主頁
+    # ex: /polls/ 主頁 tab1
     path('', views.index, name='index'),
+    # ex: /polls/api/1 房客基本資料
+    path('api/<int:cust_id>', views.api),    
     # ex: /polls/list/5 繳費清單
     path('list/<int:dashboard_id>', views.list, name='list'),
     # ex: /polls/api/bill/5 待繳費
     path('api/bill/<int:bill_id>', views.bill, name='bill'),
 
-    # ex: /polls/electric/
+    # ex: /polls/electric/ 公共電費 tab2
     path('electric/', views.electric, name='electric'),
-    # ex: /polls/electric/B1
+    # ex: /polls/electric/B1 公共電費清單
     path('api/electric/<str:dashboard_id>', views.electricList,name='electricList'),
 
-    # ex: /polls/api/1
-    path('api/<int:cust_id>', views.api),
 
 
 ]
--- polls/views.py
+++ polls/views.py
@@ -10,13 +10,18 @@ from .models import DTSF01,DTSF02,DTSF03,DTSF04
 def index(request):
     DTSF01_list = DTSF01.objects.order_by("ROOM")
     return render(request, 'polls/index.html', locals())
+# ex: /polls/api/1 房客基本資料
+def api(request,cust_id):
+	DTSF01_object = DTSF01.objects.get(pk=cust_id)
+	data = serializers.serialize('json', [DTSF01_object,])
+	return JsonResponse(data, safe=False)	
 # ex: /polls/list/5 繳費清單
 def list(request, dashboard_id):
     DTSF01vo = get_object_or_404(DTSF01, pk=dashboard_id)
     DTSF02_list = DTSF01vo.dtsf02_set.order_by("-INPUT_DATE")
     print(DTSF02_list)
     return render(request, 'polls/detail.html', locals())
-# ex: /polls/api/bill/5 待繳費
+# ex: /polls/api/bill/5 待繳費→已繳費
 def bill(request,bill_id):
 	downtown_store  = DTSF02.objects.get(pk=bill_id)
 	downtown_store.IS_CONF = True
@@ -24,21 +29,14 @@ def bill(request,bill_id):
 	data = {'STATUS': "OK" }
 	return JsonResponse(data)
 
-
+# ex: /polls/electric/ 公共電費
 def electric(request):
     DTSF03_list = DTSF03.objects.order_by("DASHBOARD")
     return render(request, 'polls/index2.html', locals())    
-
+# ex: /polls/electric/B1 公共電費清單
 def electricList(request,dashboard_id):
 	print("dashboard_id",dashboard_id)
 	DTSF04_object = DTSF04.objects.filter(DASHBOARD=dashboard_id).order_by("-INPUT_DATE")[:10]
 	return JsonResponse(serializers.serialize('json', DTSF04_object), safe=False)
 
-
-
- 
-
-def api(request,cust_id):
-	DTSF01_object = DTSF01.objects.get(pk=cust_id)
-	data = serializers.serialize('json', [DTSF01_object,])
-	return JsonResponse(data, safe=False)		
+	
