8ab02ac2085aeb1a5c69651d0075b229cbc33c60
smallken99 <smallken@gmail.com>
2018-10-28 21:44:44 +0800

    move static dirtctory

--- mysite/settings.py
+++ mysite/settings.py
@@ -55,7 +55,8 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        # 'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
  
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR,'static'),
-    )
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR,'static'),
+#     )
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
